7b0ad52d46fbb52a72010a97403168981efc8d23
Ermuxan <ermuxan747@gmail.com>
2025-07-06 18:31:42 +0300

    updated start.py file

--- start.py
+++ start.py
@@ -1,21 +1,22 @@
 import asyncio
-from fastapi import FastAPI
+import uvicorn
 from web.main import app as fastapi_app
 from bot.main import bot, dp
+from aiogram import Dispatcher
 
 async def start_bot():
-    from aiogram import Dispatcher
     await dp.start_polling(bot)
 
-def create_app():
-    app = fastapi_app
-    return app
-
-# Asynchronous run (FastAPI + Bot)
-if __name__ == "__main__":
-    import uvicorn
-
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_bot())
+async def main():
+    # Parallel ishga tushurish (FastAPI va Bot)
+    bot_task = asyncio.create_task(start_bot())
     
-    uvicorn.run("start:create_app", host="0.0.0.0", port=8001, reload=False)
+    config = uvicorn.Config(app=fastapi_app, host="0.0.0.0", port=8001)
+    server = uvicorn.Server(config)
+
+    api_task = asyncio.create_task(server.serve())
+    
+    await asyncio.gather(bot_task, api_task)
+
+if __name__ == "__main__":
+    asyncio.run(main())
