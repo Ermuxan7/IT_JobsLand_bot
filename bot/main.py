f3c0fb93ec0789f4cf702083900680d56b91929b
Ermuxan <ermuxan747@gmail.com>
2025-07-07 11:17:34 +0300

    updated start files

--- bot/main.py
+++ bot/main.py
@@ -1,21 +1,15 @@
-import asyncio
+
 from aiogram import Bot, Dispatcher
 from bot.config import BOT_TOKEN
 from bot.handlers import start, callbacks
 from web.db import database
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
 dp.include_router(start.router)
 dp.include_router(callbacks.router)
 
-async def main():
-    await database.connect()
-    try:
-        await dp.start_polling(bot)
-    finally:
-        await database.disconnect()
 
 # if __name__ == "__main__":
 #     asyncio.run(main())
--- start.py
+++ start.py
@@ -1,14 +1,17 @@
-# start.py
-
 import asyncio
 import os
 import uvicorn
 from web.main import app as fastapi_app
 from bot.main import bot, dp
+from web.db import database
 
 async def start_bot():
     print("✅ Starting polling...")
-    await dp.start_polling(bot)
+    await database.connect()
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await database.disconnect()
 
 async def start_api():
     port = int(os.environ.get("PORT", 8080))
@@ -16,7 +19,6 @@ async def start_api():
     server = uvicorn.Server(config)
     await server.serve()
 
-# 💡 BU MUHIM! Railway 'start:app' ni mana shu funksiya sifatida kutmoqda:
 async def app():
     await asyncio.gather(
         start_api(),
