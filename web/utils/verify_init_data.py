65afe8885a55d0f3033e6de48fc1c4f0b9ba94a0
Ermuxan <ermuxan747@gmail.com>
2025-07-09 12:26:29 +0300

    updated verify_init_data file

--- web/utils/verify_init_data.py
+++ web/utils/verify_init_data.py
@@ -9,39 +9,23 @@ def verify_init_data(init_data: str) -> dict | None:
             print("⚠️ init_data bos.")
             return None
 
-        parsed = urllib.parse.parse_qs(init_data, strict_parsing=True)
-        
-        hash_check = parsed.get('hash', [None])[0]
+        parsed = dict(urllib.parse.parse_qsl(init_data, strict_parsing=True))
+        hash_check = parsed.pop('hash', None)
         if not hash_check:
             print("⚠️ hash joq.")
             return None
 
-        # 'hash' dan boshqa barcha key=value
-        data_check_arr = []
-        for key in sorted(parsed):
-            if key != 'hash':
-                value_list = parsed.get(key)
-                if not value_list or not value_list[0]:
-                    print(f"⚠️ Kalit {key} qiymati yo‘q.")
-                    return None
-                value = value_list[0]
-                data_check_arr.append(f"{key}={value}")
+        data_check_arr = [f"{k}={v}" for k, v in sorted(parsed.items())]
         data_check_string = '\n'.join(data_check_arr)
 
-        # HMAC hash tekshiruvi
         secret_key = hashlib.sha256(BOT_TOKEN.encode()).digest()
         hmac_hash = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256).hexdigest()
 
         if hmac_hash != hash_check:
-            print("⚠️ HMAC hash qate.")
+            print("⚠️ HMAC hash qa'te.")
             return None
 
-        # Ma’lumotlar valid
-        return {
-            key: parsed[key][0]
-            for key in parsed
-            if key != 'hash'
-        }
+        return parsed
 
     except Exception as e:
         print("❌ verify_init_data ERROR:", e)
